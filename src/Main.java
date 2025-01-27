6a451381f39a0ef3f075f2783d496ccef365834b
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2025-01-27 14:47:48 -0300

    aplicando herança no código

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
-        Filme filmeUm;
-        filmeUm = new Filme();
+        Filme filmeUm = new Filme();
+        Serie lost = new Serie();
 
         filmeUm.setNome("Carros");
         filmeUm.setAnoDeLancamento(2008);
         filmeUm.setCategoria("Animação");
         filmeUm.setTempoDeDuracao(105);
         filmeUm.setIncluidoNoPlano(true);
+        filmeUm.setDiretor("Tarantino");
 
         filmeUm.exibeFichaTecnica();
 
@@ -20,5 +22,14 @@ public class Main {
         System.out.println();
         System.out.println(filmeUm.getTotalDeAvaliacoes());
         System.out.println(filmeUm.pegaMedia());
+
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2007);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        
+
+
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -2,50 +2,14 @@ package br.com.alura.screenmatch.modelos;
 
 import org.w3c.dom.ls.LSOutput;
 
-public class Filme {
-    private String nome;
-    private String categoria;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int tempoDeDuracao;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setTempoDeDuracao(int tempoDeDuracao) {
-        this.tempoDeDuracao = tempoDeDuracao;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.printf("Nome do Filme: %s", nome);
-        System.out.printf("\nAno de lançamento: %d", anoDeLancamento);
-    }
-
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
